c8285cb78303439e29e4663b3af4be1614da0305
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-20 17:05:42 +0300

    fix animation

--- app/src/main/java/com/the_chance/pizzaanimation/screen/composable/LazyRow.kt
+++ app/src/main/java/com/the_chance/pizzaanimation/screen/composable/LazyRow.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -21,45 +20,45 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import com.the_chance.pizzaanimation.screen.IngredientUiState
+import com.the_chance.pizzaanimation.screen.Pizza
 import com.the_chance.pizzaanimation.screen.PizzaUiState
 import com.the_chance.pizzaanimation.screen.PizzaViewModel
-import com.the_chance.pizzaanimation.ui.theme.BackgroundColor
 import com.the_chance.pizzaanimation.ui.theme.space32
 
 @Composable
 fun LazyRawIngredient(
-    viewModel: PizzaViewModel = hiltViewModel()
+    viewModel: PizzaViewModel = hiltViewModel(),
+    pizza: Int,
 ) {
     val state by viewModel.state.collectAsState()
-    val selectedIngredient = viewModel.selectedIngredient
+//    val selectedIngredient = viewModel::onSelectIngredient
 
-    LazyRawContent(state = state){ selectedIngredient ->
-        viewModel.setSelectedIngredient(selectedIngredient)}
+    LazyRawContent(state = state.pizzas[pizza])
 }
 
 @Composable
 private fun LazyRawContent(
     state: PizzaUiState,
-    onItemClick: (IngredientUiState) -> Unit
 ) {
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp),
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(24.dp),
         contentPadding = PaddingValues(horizontal = 16.dp)
     ) {
-        items(state.ingredients) {
-            IngredientItem(state = it, onItemClick)
+        items(5) {
+            IngredientItem(state = state, it)
         }
     }
 }
 
 @Composable
-fun IngredientItem(state: IngredientUiState, onItemClick: (IngredientUiState) -> Unit) {
+fun IngredientItem(state: PizzaUiState , index: Int) {
     Card(
         modifier = Modifier,
         RoundedCornerShape(space32),
         colors = CardDefaults.cardColors(Color.White)
     ) {
         Image(
-            painter = rememberAsyncImagePainter(model = state.image),
+            painter = rememberAsyncImagePainter(model = state.ingredients[index]),
             contentDescription = null,
             modifier = Modifier
                 .size(65.dp)
