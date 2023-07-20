82c39352c2456688d8dbd2a7b60fbfa9e8921d7e
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-20 09:16:55 +0300

    add in dimen , strings, drawable

---
+++ app/src/main/java/com/the_chance/pizzaanimation/ui/theme/Dimens.kt
@@ -0,0 +1,45 @@
+package com.the_chance.pizzaanimation.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+//region Space Dimensions
+
+val space0 = 0.dp
+val zero = 0.dp
+val space2 = 2.dp
+val space4 = 4.dp
+val space8 = 8.dp
+val space10 = 10.dp
+val space16 = 16.dp
+val space24 = 24.dp
+val space32 = 32.dp
+val space40 = 40.dp
+val space46 = 46.dp
+val space48 = 48.dp
+val space56 = 56.dp
+val space64 = 64.dp
+val space72 = 72.dp
+val space80 = 80.dp
+val space100= 100.dp
+val space170 = 170.dp
+val space200= 200.dp
+val space480= 500.dp
+
+
+//endregion
+
+//region Text Size Dimensions
+val textSize0 = 0.sp
+val textSize12 = 12.sp
+val textSize14 = 14.sp
+val textSize16 = 16.sp
+val textSize18 = 18.sp
+val textSize20 = 20.sp
+val textSize24 = 24.sp
+val textSize32 = 32.sp
+val textSize54 = 54.sp
+
+//endregion
+
+
